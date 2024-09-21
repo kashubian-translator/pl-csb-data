e631e5d9654fcaa509bd0fefa2c6dd9b2295c056
jan-Kulpas <102905054+jan-Kulpas@users.noreply.github.com>
2024-09-21 08:33:08 +0200

    CSB-3 Change data column names to match pl-csb-model

--- pl-csb-data/data_preparer.py
+++ pl-csb-data/data_preparer.py
@@ -2,22 +2,25 @@ import os
 import pandas as pd
 from typing import Tuple
 
+
 def read_text_file(filename: str) -> list:
     with open(filename, "r", encoding="utf-8") as file:
         return [line.strip("\n") for line in file.readlines()]
 
-def prepare_translation_dataset(source_path: str, target_path: str) -> Tuple[pd.DataFrame, pd.DataFrame]:
-    kashubian_train = read_text_file(target_path)
-    polish_train = read_text_file(source_path)
+
+def prepare_translation_dataset(source_path: str, target_path: str, source_lang: str, target_lang: str) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    source_train = read_text_file(source_path)
+    target_train = read_text_file(target_path)
 
     train_data = []
-    for kashubian, polish in zip(kashubian_train, polish_train):
-        train_data.append([kashubian.strip(), polish.strip()])
+    for source, target in zip(target_train, source_train):
+        train_data.append([source.strip(), target.strip()])
 
-    train_df = pd.DataFrame(train_data, columns=["pl", "csb"])
+    train_df = pd.DataFrame(train_data, columns=[source_lang, target_lang])
 
     return train_df
 
-def prepare(source_path: str, target_path: str, output_path: str) -> None:
-    train_df = prepare_translation_dataset(source_path, target_path)
+
+def prepare(source_path: str, target_path: str, output_path: str, source_lang: str, target_lang: str) -> None:
+    train_df = prepare_translation_dataset(source_path, target_path, source_lang, target_lang)
     train_df.to_csv(output_path, sep="\t")
