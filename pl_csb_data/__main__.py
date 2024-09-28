4808b6f28f3eade0c15c9e8656241a74bc5e7cb4
Noxxxxious <michal.wrzosek.robert@gmail.com>
2024-09-29 01:40:05 +0200

    Fix config path

--- pl_csb_data/__main__.py
+++ pl_csb_data/__main__.py
@@ -1,24 +1,31 @@
 import config_loader
-import data_preparer
-import data_normalizer
+from data_preparer import DataPreparer
+from data_normalizer import DataNormalizer
+from logging import Logger
+from logger import set_up_logger
 
 
-def process_data(data_paths: dict, language: dict) -> None:
-    data_preparer.prepare(
+def process_data(data_paths: dict, language: dict, logger: Logger) -> None:
+    DataPreparer(logger).prepare(
         data_paths["source_file"],
         data_paths["target_file"],
         data_paths["output_file"],
         language["source_language"],
         language["target_language"]
     )
-    data_normalizer.normalize(
+    DataNormalizer(logger).normalize(
         data_paths["output_file"],
         data_paths["output_file"]
     )
 
 
 if __name__ == "__main__":
-    config = config_loader.load()
+    logger = set_up_logger(__name__, "INFO")
 
-    process_data(config["TRAINING"], config["LANGUAGE"])
-    process_data(config["EVALUATION"], config["LANGUAGE"])
+    config = config_loader.load("pl_csb_data/config.ini", logger)
+
+    logger.info("Processing training data")
+    process_data(config["TRAINING"], config["LANGUAGE"], logger)
+
+    logger.info("Processing evaluation data")
+    process_data(config["EVALUATION"], config["LANGUAGE"], logger)
